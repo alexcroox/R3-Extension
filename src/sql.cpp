9071e2290b191afb13c31bc530f1038a7e37000a
László Major <laszlo.major@outlook.com>
2017-10-27 21:50:48 +0200

    On duplicate insert for infantry commands.

--- src/sql.cpp
+++ src/sql.cpp
@@ -103,7 +103,12 @@ namespace {
         escapeAndAddStringToQueryWithComa(unitWeapon, query);
         escapeAndAddStringToQueryWithComa(unitLauncher, query);
         escapeAndAddStringToQueryWithComa(unitData, query);
-        query << missionTime << "),";
+        query << missionTime << ")";
+        query << " ON DUPLICATE KEY UPDATE player_id = ";
+        escapeAndAddStringToQuery(playerId, query);
+        query << ", name = ";
+        escapeAndAddStringToQuery(unitName, query);
+        query << ";";
     }
 
     void processInfantryPositionsCommand(std::ostringstream& query, const std::vector<std::string>& params) {
